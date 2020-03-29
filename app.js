3480c600e5093ff9f697eebbf9bc4a80ab961189
Andriy Trofimov <webgrandmo@gmail.com>
2020-03-29 20:56:59 +0300

    cinema-seats/js. Implement localStorage functionality

--- app.js
+++ app.js
@@ -6,6 +6,22 @@
         total = document.getElementById('total'),
         movieSelect = document.getElementById('movie-selector');
     let ticketPrice = +movieSelect.value;
+    
+    populateUI();
+    
+    function populateUI() {
+        const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+        if(selectedSeats && selectedSeats.length) {
+            seats.forEach( (seat, index) => {
+                if(selectedSeats.indexOf(index) > -1) {
+                    seat.classList.add('selected');
+                }
+            });
+        }
+
+        const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 
     function chooseSeat(event) {
         checkIfSeat(event);
@@ -17,19 +33,29 @@
 
         updateCountAndPrice();
     }
+    function saveMovies(movieSelector, moviePrice) {
+        localStorage.setItem('selectedMovieIndex', movieSelector);
+        localStorage.setItem('selectedMoviePrice', moviePrice);
+    }
     function updateCountAndPrice() {
         const selectedSeats = document.querySelectorAll('.row .seat.selected');
         const selectedCount = selectedSeats.length;
         count.innerText = selectedCount;
         total.innerText = selectedCount * ticketPrice;
+
+        const seatIndex = [...selectedSeats].map( seat => [...seats].indexOf(seat));
+        localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
     }
 
     function updateMoviePrice(e) {
         ticketPrice = e.target.value;
+        saveMovies(e.target.selectedIndex, e.target.value);
         updateCountAndPrice();
     }
 
     container.addEventListener('click', chooseSeat);
-    movieSelect.addEventListener('change', updateMoviePrice)
+    movieSelect.addEventListener('change', updateMoviePrice);
 
-}())
+    updateCountAndPrice();
+
+}());
